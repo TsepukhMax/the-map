c666c8efacab31f59ad3ff2f6a241ef5472ebce7
TsepukhMax <163754175+TsepukhMax@users.noreply.github.com>
2025-03-24 21:09:45 +0200

    added initializeMap

--- src/app/main-module/components/map/map.component.ts
+++ src/app/main-module/components/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './map.component.scss',
   standalone: false,
 })
-export class MapComponent {}
+export class MapComponent implements OnInit {
+  ngOnInit() {
+    this.initializeMap();
+  }
+
+  initializeMap() {
+    // Створюємо карту
+    const map = L.map('map').setView([51.505, -0.09], 13); // координати та зум
+
+    // Додаємо плитки (ти можеш змінити на бажане джерело)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Додаємо маркер
+    L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('<b>Hello world!</b><br />I am a popup.')
+      .openPopup();
+  }
+}
